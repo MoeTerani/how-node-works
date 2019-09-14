faa7f13fdbd74b7c42e09285bc054bc76c44d8c7
MoeTerani <moe.terani@gmail.com>
2019-09-14 11:10:01 +0200

    Events-final-simulated server listener

--- event.js
+++ event.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 //this the best practice tyo use class to create emit events
 class Sale extends EventEmitter {
@@ -30,3 +31,25 @@ myEmitter.emit('newSale', 9); // #2 Create a new event
 
 //# After, an empty EventEmitter object
 console.log(myEmitter);
+
+//**************************************************************** */
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log(req.url);
+  console.log('Request Recieved');
+  res.end('Request Recieved');
+});
+
+server.on('request', (req, res) => {
+  console.log(' Another Request 👌🏼');
+});
+
+server.on('close', (req, res) => {
+  res.end(' Server closed');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for request');
+});
